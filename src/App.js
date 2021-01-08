0871179b84203460d8d0903cc6d14bc08e3ee6bc
Sufyan Raza <34751328+Sufyanraza50@users.noreply.github.com>
2021-01-08 20:21:44 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import InfoPanal from './components/InfoPanal';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <InfoPanal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InfoPanal.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 1000,
+        margin: '0 auto',
+        marginTop: 50
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    title: {
+        color: '#3f51b5'
+    }
+}));
+
+export default function InfoPanal() {
+    const classes = useStyles();
+
+    const [globalData, setGlobalData] = useState({});
+
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch("https://api.covid19api.com/summary");
+            let data = await response.json();
+            setGlobalData(data.Global)
+            console.log(data.Global);
+        }
+        getData();
+    }, [])
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                {
+                    Object.keys(globalData).map((key, ind) => {
+                        return (
+                            <Grid item xs={12} sm={4} key={ind}>
+                                <Paper className={classes.paper} elevation={3}>
+                                    <h3 className={classes.title}>{key.split(/(?=[A-Z])/).join(" ").toUpperCase()}</h3>
+                                    <h3>{globalData[key]}</h3>
+                                </Paper>
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        </div>
+    );
+}
